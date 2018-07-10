b066da8a82fc79a2ff641df0103d2f709b40df6e
lmacleod36 <lmacleod36@gmail.com>
2018-07-09 21:20:26 -0700

    Game tracks logic to determin tic tac toe winner

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -5,14 +5,21 @@ export class Board extends React.Component <any, any> {
     constructor(props: any){
         super(props);
         this.state = { 
-            squares: ['','','','','','','', '','']
+            squares: ['','','','','','','', '',''],
+            xIsNext: true
         };
     }
 
     handleClick(i: any) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+          }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
       }
 
   renderSquare(i : any ) {
@@ -23,7 +30,13 @@ export class Board extends React.Component <any, any> {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -47,3 +60,23 @@ export class Board extends React.Component <any, any> {
     );
   }
 }
+
+function calculateWinner(squares: any) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
