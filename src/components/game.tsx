d88b06e6c733272165d4dc2799b9fc6a1319d3c4
lmacleod36 <lmacleod36@gmail.com>
2018-07-28 12:45:07 -0700

    Updating game config management

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Board } from './board';
 import * as Utils from './utils';
 
+
 export class Game extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
@@ -9,11 +10,17 @@ export class Game extends React.Component<any, any> {
       history: [{
         squares: Utils.initSquareValues,
       }],
-      groupings: Utils.initSquareGroupings,
-      groupMagicNumber: Math.floor(Math.random() * Utils.groupings),
-      lives: 4,
+      level_id: 1,
+      groupings: Utils.json_config.levels[1].dataGroupings,
+      groupMagicNumber: this.pickWinner(),
+      lives: Utils.maxLives,
       status: "Fail"
     };
+
+  }
+
+  pickWinner(){
+    return Math.floor(Math.random() * Utils.json_config.levels[this.state.level_id].groupingsCount);
   }
 
   handleClick(i: any) {
@@ -21,13 +28,13 @@ export class Game extends React.Component<any, any> {
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     const lives = this.state.lives;
-    
+
     let newStatus = this.state.status;
 
-    if(Utils.isMatch(this.state.groupMagicNumber, this.state.groupings[i])){
+    if (Utils.isMatch(this.state.groupMagicNumber, this.state.groupings[i])) {
       newStatus = "Winner";
     }
-    else{
+    else {
       newStatus = "Fail";
     }
 
@@ -42,8 +49,8 @@ export class Game extends React.Component<any, any> {
     });
   }
 
-  removeSquare(){
-    
+  putSquareValue() {
+
   }
 
   render() {
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {Game} from './components/game';
+import * as Utils from './components/utils';
 
 ReactDOM.render(
   <Game />,
